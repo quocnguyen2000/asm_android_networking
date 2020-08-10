ce7d81af15c04d4392cf21d12506fe0ea42a589e
=Nguyen_dinh_quoc <=nguyendinhquoc.dev@gmail.com>
2020-08-10 07:57:48 +0700

    nguyendinhquoc_ps10377_assignment

---
+++ app/src/main/java/com/example/ps10377_android_app/Constants.java
@@ -0,0 +1,15 @@
+package com.example.ps10377_android_app;
+
+/**
+ * Created by Asus on 07/02/2018.
+ */
+
+public class Constants {
+    public static final String URL= "http://192.168.1.37/";
+    public static final String ROOT_URL = URL+"android/crud_api/";
+    public static final String URL_ADD = ROOT_URL+"add.php";
+    public static final String URL_DELETE = ROOT_URL+"delete.php";
+    public static final String URL_UPDATE = ROOT_URL+"update.php";
+    public static final String URL_SELECT = ROOT_URL+"select.php";
+    public static final String URL_SELECT_ID = ROOT_URL+"select_id.php";
+}
